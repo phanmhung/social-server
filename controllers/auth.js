907061f65b9c52d7a7a17ab77cdcd8c94c929cae
Phan M. Hung <pmhungpro1@gmail.com>
2022-11-30 15:55:07 +0700

    refactor: add login

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,6 +7,39 @@ const register = async(req,res)=>{
 
 }
 
+const login = async(req,res)=>{
+    const {email,password, rememberPassword} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({message:"All fields are required"});
+    }
+
+    if(password.length < 8){
+        return res.status(400).json({message:"Password must be at leaast 8 characters"});   
+    }
+
+    if(!validator.isEmail(email)){
+        return res.status(400).json({message:"Invalid email"});
+    }
+    
+    const user = await User.findOne({email})
+    if(!user){
+        return res.status(400).json({message:"Invalid email or password"});
+    }
+
+    const isMatch = await user.matchPassword(password);
+    if(!isMatch){
+        return res.status(400).json({message:"Invalid email or password"});
+    }
+
+    const token = jwt.sign({_id:user._id},process.env.JWT_SECRET,{
+        expiresIn: rememberPassword ? "365d" : process.env.JWT_LIFETIME
+    });
+
+    return res.status(200).json({message:"Login successful",token});
+};
+
 export {
-    register
+    register,
+    login
 };
